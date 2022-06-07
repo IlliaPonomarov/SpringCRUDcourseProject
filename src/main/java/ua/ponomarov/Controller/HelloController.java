a9666d22aa9c40a794b98c4c609fba379638f6f2
Illia <chippaxyucac@gmail.com>
2022-06-08 00:15:57 +0200

    Lesson 18

--- src/main/java/ua/ponomarov/Controller/HelloController.java
+++ src/main/java/ua/ponomarov/Controller/HelloController.java
@@ -2,14 +2,21 @@ package ua.ponomarov.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/first")
 public class HelloController {
 
 
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello_world";
+        return "hello/hello_world";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbye(){
+        return "hello/goodbye";
     }
 }
