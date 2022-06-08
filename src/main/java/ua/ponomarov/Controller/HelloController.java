4a2804d86c438026f3c5233fda553129b00b15a5
Illia <chippaxyucac@gmail.com>
2022-06-08 13:01:52 +0200

    Lesson 21

--- src/main/java/ua/ponomarov/Controller/HelloController.java
+++ src/main/java/ua/ponomarov/Controller/HelloController.java
@@ -1,22 +1,33 @@
 package ua.ponomarov.Controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/first")
 public class HelloController {
 
 
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello(@RequestParam(value = "name", required = false) String name,
+                        @RequestParam(value = "secondname", required = false) String surname,
+                        Model model){
+
+        model.addAttribute("text", "Hello " + name + "\t" + surname);
         return "hello/hello_world";
     }
 
     @GetMapping("/goodbye")
-    public String goodbye(){
+    public String goodbye(@RequestParam(value = "name", required = false) String name,
+                          @RequestParam(value = "secondname", required = false) String surname,
+                          Model model) {
+        model.addAttribute("text", "Hello " + name + "\t" + surname);
         return "hello/goodbye";
     }
 }
