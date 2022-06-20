69ef2a78dbc149f79e33ed23ca743f754d5012a4
Illia <chippaxyucac@gmail.com>
2022-06-20 19:48:05 +0200

    Lesson 39

--- src/main/java/ua/ponomarov/DAO/PersonDAO.java
+++ src/main/java/ua/ponomarov/DAO/PersonDAO.java
@@ -27,7 +27,7 @@ public class PersonDAO {
     }
 
     public void save(Person person){
-        jdbcTemplate.update("INSERT INTO people VALUES (1, ?, ?, ?)",
+        jdbcTemplate.update("INSERT INTO people(name, age, email) VALUES (?, ?, ?)",
                 person.getName(),
                 person.getAge(),
                 person.getEmail());
@@ -60,7 +60,7 @@ public class PersonDAO {
 
 
         for (Person p : people) {
-            jdbcTemplate.update("INSERT INTO people VALUES(?, ?, ?, ?)", p.getId(), p.getName(), p.getAge(), p.getEmail());
+            jdbcTemplate.update("INSERT INTO people(name, age, email) VALUES?, ?, ?)", p.getName(), p.getAge(), p.getEmail());
         }
 
         long after = System.currentTimeMillis();
@@ -74,13 +74,12 @@ public class PersonDAO {
 
         long before = System.currentTimeMillis();
 
-            jdbcTemplate.batchUpdate("INSERT INTO people VALUES(?, ?, ?, ?)", new BatchPreparedStatementSetter() {
+            jdbcTemplate.batchUpdate("INSERT INTO people(name ,age, email) VALUES(?, ?, ?)", new BatchPreparedStatementSetter() {
                 @Override
                 public void setValues(PreparedStatement ps, int i) throws SQLException {
-                    ps.setInt(1, people.get(i).getId());
-                    ps.setString(2, people.get(i).getName());
-                    ps.setInt(3, people.get(i).getId());
-                    ps.setString(4, people.get(i).getEmail());
+                    ps.setString(1, people.get(i).getName());
+                    ps.setInt(2, people.get(i).getId());
+                    ps.setString(3, people.get(i).getEmail());
                 }
 
                 @Override
